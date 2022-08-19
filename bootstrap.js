33939a41554471ffd614eeb3b7a28e9154baa900
Andy Keobounphan <andykeo@live.ca>
2022-08-19 13:15:29 -0600

    Added a clickable download URL.
    You can click the link to download the PDF now.

--- bootstrap.js
+++ bootstrap.js
@@ -1,16 +1,28 @@
 import("./index.js").then(wasm => {
-  fetch('./myTemplate.json')
-    .then(response => {
-      return response.json();
-    })
-    .then(doc => {
-        console.log(doc);
-      // Change the title to show date (now)
-      let date = new Date().toLocaleString();
-      let title = doc.contents[0].params;
-      title.text += " (created: " + date + ")";
-      wasm.createPDF(doc);
-    });
+    fetch('./myTemplate.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(doc => {
+            console.log(doc);
+            // Change the title to show date (now)
+            let date = new Date().toLocaleString();
+            let title = doc.contents[0].params;
+            let contents = doc.contents[1].params;
+            // let message = document.getElementById("inputs").value;
 
-  // hello
+            title.text += " (created: " + date + ")";
+
+
+            // this is how you get parameters from your HTML
+            //let message = document.getElementById("inputs").value;
+            // console.log(message);
+
+
+            contents.text += ' You can put your inputs here';
+
+            wasm.createPDF(doc);
+        });
+
+    // hello
 }).catch(e => console.error("Error importing `index.js`:", e));
